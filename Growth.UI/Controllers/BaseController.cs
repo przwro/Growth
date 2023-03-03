7c5cebc8b0deced713180356a7bf848e97aa8ea9
wrobp0kr <przemyslaw.wrobel@walstead-ce.com>
2023-03-03 13:55:48 +0100

    Add MediatR

---
+++ Growth.UI/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Growth.UI.Controllers;
+public abstract class BaseController : Controller
+{
+    private ISender _mediator;
+    protected ISender Mediator => _mediator ??=
+        HttpContext.RequestServices.GetService<ISender>();
+}
